68881c121de96dfc04c4405b06d15b478bf6a6e8
Proshanta Kumar Roy <Proshanta.ict@pakiza.com>
2025-07-27 17:23:01 +0600

    27-07-2025

--- js/singleProduct.js
+++ js/singleProduct.js
@@ -8,63 +8,66 @@ let largeImg = document.getElementsByClassName("img-slide");
 let slideIndex = 1;
 showSlides(slideIndex);
 
-
-document.addEventListener('swiped', function(e) {
-    console.log(e.target); // the element that was swiped
-    console.log(e.detail.dir); // swiped direction
+document.addEventListener('swiped', function (e) {
+    console.log(e.target);
+    console.log(e.detail.dir);
 });
+
 // Next/previous controls
 function plusSlides(n) {
-    showSlides((slideIndex += n));
+    if ((n < 0 && slideIndex <= 1) || (n > 0 && slideIndex >= largeImg.length)) {
+        return; // Prevent out-of-bound navigation
+    }
+    showSlides(slideIndex += n);
     plusZoomSlides(n);
     handleChangeValue(slideIndex);
-
+    updateArrowColors();
 }
+
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides((slideIndex = n));
+    showSlides(slideIndex = n);
     currentSuperImage(n);
     currentValue = n;
     handleCurrentValue(currentValue);
+    updateArrowColors();
 }
 
 function showSlides(n) {
-    let i;
     let slides = document.getElementsByClassName("img-slide");
     let dots = document.getElementsByClassName("small-img");
+
     if (n > slides.length) {
         slideIndex = 1;
     }
     if (n < 1) {
         slideIndex = slides.length;
     }
-    for (i = 0; i < slides.length; i++) {
+
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-        slides[i].children[0].className = slides[i].children[0].className.replace(
-            " large-img",
-            ""
-        );
+        slides[i].children[0].className = slides[i].children[0].className.replace(" large-img", "");
     }
-    for (i = 0; i < dots.length; i++) {
+
+    for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    // slides[slideIndex - 1].children[0].className += " large-img";
 
-    // set media-query class "large-img" for large device
     const media = (e) => {
         if (slides[slideIndex - 1].children[0].className.includes("large-img")) {
-            slides[slideIndex - 1].children[0].className -= " large-img";
+            slides[slideIndex - 1].children[0].className = slides[slideIndex - 1].children[0].className.replace(" large-img", "");
         }
         if (e.matches) {
             slides[slideIndex - 1].children[0].className += " large-img";
         }
     };
+
     const mqLarge = window.matchMedia("(min-width: 992px)");
     media(mqLarge);
     mqLarge.addEventListener("change", media);
-    // end media-query
 
     if (slides[slideIndex - 1].children[0].className.includes("large-img")) {
         jQuery(function () {
@@ -80,36 +83,33 @@ function showSlides(n) {
             });
         });
     }
+
+    updateArrowColors();
 }
-// ==================================================
 
-/*==============================
-product image by color
-==============================*/
-// const colorSelect = document.querySelector("#form-color-select");
-// const imgSelect = document.querySelectorAll("#single-product-img img");
+function updateArrowColors() {
+    const prevArrowIcon = document.querySelector("#prevArrow i");
+    const nextArrowIcon = document.querySelector("#nextArrow i");
 
-// colorSelect.addEventListener("change", () => {
-//   const selectedValue =
-//     colorSelect.options[colorSelect.selectedIndex].value.toLowerCase();
-//   if (selectedValue) {
-//     for (let img of imgSelect) {
-//       // find domain
-//       const imgUrl = img.src.split("/").slice(0, -1).join("/");
-//       // find img color
-//       let elements = img.src.split("/").slice(-1)[0].split("-");
-//       elements[0] = selectedValue;
-//       const result = elements.join("-");
-//       img.src = `${imgUrl}/${result}`;
-//     }
-//   }
-// });
-// ============================================
+    const inactiveColor = "rgba(146, 146, 146, 0.5)";
+    const activeColor = "";
+
+    if (slideIndex <= 1) {
+        prevArrowIcon.style.color = inactiveColor;
+    } else {
+        prevArrowIcon.style.color = activeColor;
+    }
+
+    if (slideIndex >= largeImg.length) {
+        nextArrowIcon.style.color = inactiveColor;
+    } else {
+        nextArrowIcon.style.color = activeColor;
+    }
+}
 
 /*==========================
-product img view slide
+Product img view slide
 ===========================*/
-// const viewBtn = document.querySelectorAll(".product-view-btn");
 const allImgs = document.querySelectorAll(".carousel-item");
 const imgView = document.querySelector(".img-view");
 const imgBox = document.querySelector(".img-box");
@@ -120,40 +120,21 @@ let sliderCurrentImgCount = document.querySelector(".slider-current-img-count");
 let sliderTotalImgCount = document.querySelector(".slider-total-img-count");
 
 let currentImgIdx = 0;
+
 if (sliderTotalImgCount) {
-    sliderTotalImgCount.innerText = allImgs?.length;
+    sliderTotalImgCount.innerText = allImgs.length;
 }
 
 if (imgView) {
     imgView.addEventListener("click", function () {
         this.style.display = "none";
         imgBox.style.display = "none";
-
-        sliderOptions.classList.remove("d-block");
-        sliderOptions.classList.add("d-none");
-
-        bigSliderPrevBtn.classList.remove("d-block");
-        bigSliderPrevBtn.classList.add("d-none");
-        bigSliderNextBtn.classList.remove("d-block");
-        bigSliderNextBtn.classList.add("d-none");
+        sliderOptions.classList.replace("d-block", "d-none");
+        bigSliderPrevBtn.classList.replace("d-block", "d-none");
+        bigSliderNextBtn.classList.replace("d-block", "d-none");
     });
 }
 
-// viewBtn.forEach(function (btn, idx) {
-//     btn.addEventListener("click", function () {
-//         imgView.style.display = "block";
-//         imgBox.style.display = "block";
-
-//         sliderOptions.classList.remove("d-none");
-//         sliderOptions.classList.add("d-block");
-
-//         bigSliderPrevBtn.classList.remove("d-none");
-//         bigSliderPrevBtn.classList.add("d-block");
-//         bigSliderNextBtn.classList.remove("d-none");
-//         bigSliderNextBtn.classList.add("d-block");
-//         currentImgIdx = idx + 1;
-//     });
-// });
 if (bigSliderPrevBtn) {
     bigSliderPrevBtn.addEventListener("click", function () {
         currentImgIdx--;
@@ -163,7 +144,8 @@ if (bigSliderPrevBtn) {
         sliderCurrentImgCount.innerText = currentImgIdx;
     });
 }
-if (bigSliderPrevBtn) {
+
+if (bigSliderNextBtn) {
     bigSliderNextBtn.addEventListener("click", function () {
         currentImgIdx++;
         if (currentImgIdx > allImgs.length) {
@@ -173,32 +155,25 @@ if (bigSliderPrevBtn) {
     });
 }
 
-// slider close
 function bigSlideClose() {
     imgView.style.display = "none";
     imgBox.style.display = "none";
-
-    sliderOptions.classList.remove("d-block");
-    sliderOptions.classList.add("d-none");
-
-    bigSliderPrevBtn.classList.remove("d-block");
-    bigSliderPrevBtn.classList.add("d-none");
-    bigSliderNextBtn.classList.remove("d-block");
-    bigSliderNextBtn.classList.add("d-none");
+    sliderOptions.classList.replace("d-block", "d-none");
+    bigSliderPrevBtn.classList.replace("d-block", "d-none");
+    bigSliderNextBtn.classList.replace("d-block", "d-none");
 }
-// ==================================================
 
 /*================================
-related product slider
+Related product slider
 ================================*/
 function nextItem(e) {
     let relProdMain = e.parentElement.children[0];
     let relProd = relProdMain.getElementsByClassName("related-product");
     relProdMain.append(relProd[0]);
 }
+
 function prevItem(e) {
     let relProdMain = e.parentElement.children[0];
     let relProd = relProdMain.getElementsByClassName("related-product");
     relProdMain.prepend(relProd[relProd.length - 1]);
 }
-// ==================================================
